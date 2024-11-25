a2c4625ba5dfb0c1162039f76f176c40119e8e75
Saotra Fahasoavana Andrianome <saandria@c1r6s7.42antananarivo.mg>
2024-11-25 12:35:52 +0300

    m00ex02

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 15:16:52 by saandria          #+#    #+#             */
-/*   Updated: 2024/11/08 15:38:19 by saandria         ###   ########.fr       */
+/*   Updated: 2024/11/11 08:59:58 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,24 @@ Account::Account() : _accountIndex(0), _amount(0), _nbDeposits(0), _nbWithdrawal
 Account::Account( int initial_deposit )
 {
 	
+}
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+} 
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
 }
