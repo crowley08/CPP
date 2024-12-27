55540cdfba22880fd0c9e239bf74a13d2e6c474c
Saotra Fahasoavana Andrianome <saandria@c1r9s6.42antananarivo.mg>
2024-12-27 11:18:49 +0300

    rectif main.cpp

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 10:47:19 by saandria          #+#    #+#             */
-/*   Updated: 2024/12/27 10:48:17 by saandria         ###   ########.fr       */
+/*   Updated: 2024/12/27 11:18:23 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ int	main( void )
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
-	
+
 	c = b;
-	
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
