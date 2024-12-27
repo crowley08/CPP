58088d8eea88f5b9458ab7b421d70605a91234b8
Saotra Fahasoavana Andrianome <saandria@c1r9s6.42antananarivo.mg>
2024-12-27 13:11:41 +0300

    test main

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 10:47:19 by saandria          #+#    #+#             */
-/*   Updated: 2024/12/27 11:18:23 by saandria         ###   ########.fr       */
+/*   Updated: 2024/12/27 13:11:15 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,28 @@
 
 int	main( void )
 {
-	Fixed a;
-	Fixed b( a );
-	Fixed c;
+	{
+		Fixed a;
+		Fixed b( a );
+		Fixed c;
 
-	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+		c = b;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
+	std::cout << "**********************************************" << std::endl;
+	{
+		Fixed a;
+		Fixed b( a );
+		Fixed c( b);
+		Fixed d;
+
+		d = c;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+		std::cout << d.getRawBits() << std::endl;
+	}
 	return (0); 
 }
