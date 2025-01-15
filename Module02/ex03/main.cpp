03c102359a19eaa39ad1f8c6d95bf7f4c105a393
Saotra Fahasoavana Andrianome <saandria@c1r7s5.42antananarivo.mg>
2025-01-15 13:10:12 +0300

    tsy aiko tsony ze momba anty

--- Module02/ex03/main.cpp
+++ Module02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 10:47:19 by saandria          #+#    #+#             */
-/*   Updated: 2024/12/30 08:28:06 by saandria         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:09:23 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ bool	bsp(const Point a, const Point b, const Point c, const Point point);
 
 int	main()
 {
-	Point a( 0, 5.0f);
-	Point b( 1.0f, 3.0f);
-	Point c(2.0f, 7.0f);
-	Point x(1.0f, 9.0f);
-
-	if (bsp (a, b , c, x) == true)
-		std::cout << "X belongs to triangle ABC" << std::endl;
-	else
-		std::cout << "X doesn't belong to triangle ABC" << std::endl;
+	{		
+		Point a( 0, 5.0f);
+		Point b( 1.0f, 3.0f);
+		Point c(2.0f, 7.0f);
+		Point x(1.0f, 9.0f);
+	
+		if (bsp (a, b , c, x) == true)
+			std::cout << "X belongs to triangle ABC" << std::endl;
+		else
+			std::cout << "X doesn't belong to triangle ABC" << std::endl;
+	}
 	return ( 0 );
 }
